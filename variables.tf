897022d1ad04387e73ab349a4925c6faea887e1e
Federico Gimenez <federico.gimenez@gmail.com>
2020-04-24 11:23:56 +0200

    add missing credentials env vars [skip CI]

--- variables.tf
+++ variables.tf
@@ -1,8 +1,17 @@
 variable "client_id" {
   default = "client_id"
 }
+
 variable "client_secret" {
   default = "client_secret"
+}
+
+variable "tenant_id" {
+  default = "tenant_id"
+}
+
+variable "subscription_id" {
+  default = "subscription_id"
 }
 
 variable "cluster_name" {
